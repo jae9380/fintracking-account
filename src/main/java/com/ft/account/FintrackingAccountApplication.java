19bca6168854e540d642a99d4ba42b29c1534753
jae9380 <ljy5314@gmail.com>
2026-04-16 17:33:38 +0900

    Fintracking Account Server

---
+++ src/main/java/com/ft/account/FintrackingAccountApplication.java
@@ -0,0 +1,13 @@
+package com.ft.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintrackingAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FintrackingAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ft/account/FintrackingAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ft.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackingAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
